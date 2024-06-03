aa1fd4017ab353a80cf4561166e4631c9a6cab86
Lucas Florentino <lucas7florentino@hotmail.com>
2024-06-03 18:28:01 -0300

    P2 done

--- source/projeto_final.py
+++ source/projeto_final.py
@@ -1,20 +1,5 @@
 from manim import *
 
-# class Pith(Scene):
-#     def construct(self):
-
-#         sq = Square( side_length=5, stroke_color=GREEN, fill_color=BLUE, fill_opacity=0.75)
-
-#         self.play(Create(sq), run_time=4)
-#         self.wait()
-
-
-# class CreateCircle(Scene):
-#     def construct(self):
-#         circle = Circle()  # create a circle
-#         circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
-#         self.play(Create(circle))  # show the circle on screen
-
 # class SquareToCircle(Scene):
 #     def construct(self):
 #         circle = Circle()  # create a circle
@@ -26,17 +11,6 @@ from manim import *
 #         self.play(Create(square))  # animate the creation of the square
 #         self.play(Transform(square, circle))  # interpolate the square into the circle
 #         self.play(FadeOut(square))  # fade out animation
-
-# class SquareAndCircle(Scene):
-#     def construct(self):
-#         circle = Circle()  # create a circle
-#         circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
-
-#         square = Square()  # create a square
-#         square.set_fill(BLUE, opacity=0.5)  # set the color and transparency
-
-#         square.next_to(circle, RIGHT, buff=5)  # set the position
-#         self.play(Create(circle), Create(square))  # show the shapes on screen
 
 
 # class AnimatedSquareToCircle(Scene):
@@ -82,53 +56,41 @@ from manim import *
 #         self.wait(0.5)  # wait for 0.5 seconds
 #         self.replacement_transform()
 
-# Create a class that writes text on the screen
-# class WriteText(Scene):
-#     def construct(self):
-#         text = Text("Creating a Decision Tree")  # create the text
-#         self.play(Write(text))  # write the text on screen
-#         self.wait(2)  # wait for 3 seconds
-#         self.play(FadeOut(text))  # fade out the text
-
-# Create a class that calls the WriteText class, and the creates a decision tree
-# class DecisionTree(Scene):
-#     def construct(self):
-#         text = Text("Creating a Decision Tree")
-#         self.play(Write(text))
-#         self.wait(2)
-#         self.play(FadeOut(text))
-
-#         decision_tree = Rectangle(height=6, width=8)  # create a rectangle
-#         decision_tree.set_fill(BLUE, opacity=0.5)
-#         self.play(Create(decision_tree))
-
-#         self.wait(2)
-#         self.play(FadeOut(decision_tree))
-
 
 # Create a class that calls the DecisionTree class, and the creates a decision tree with a root node
 class CreateDecisionTree(Scene):
     def construct(self):
         # Parte 1 - Introdução
-        self.create_text("Criando uma Árvore de Decisão", UP*0.5)
-        
+        text_intro = Text("Criando uma Árvore de Decisão")
+        self.play(Write(text_intro))
+        self.wait()
+        self.play(text_intro.animate.scale(0.5))
+        self.play(text_intro.animate.to_edge(UP * 0.5))
+
         plane = self.create_graph()
-        points = [(140, 2.15, RED), (130, 1.70, BLUE), (79, 1.70, GREEN), (65, 1.72, GREEN), (120, 1.70, BLUE), (125, 1.75, BLUE), (120, 2.05, RED), (75, 1.79, GREEN), (115, 1.95, RED)]
+        points = [
+            (140, 2.15, RED),
+            (130, 1.70, BLUE),
+            (79, 1.70, GREEN),
+            (65, 1.72, GREEN),
+            (120, 1.70, BLUE),
+            (125, 1.75, BLUE),
+            (120, 2.05, RED),
+            (75, 1.79, GREEN),
+            (115, 1.95, RED),
+        ]
         self.add_points_to_graph(plane, points)
 
         self.create_fisrt_tree(points)
 
         # Parte 2 - Como funciona
-        
+        self.clear_screen()
+        self.wait(0.5)
+        impurity_text = Text("Medidas de Impureza").shift(UP * 3)
+        self.play(Write(impurity_text))
+        self.wait(0.5)
 
-
-    def create_text(self, text_input, position):
-        text = Text(text_input)
-        self.play(Write(text))
-        self.wait()
-        self.play(text.animate.scale(0.5))
-        self.play(text.animate.to_edge(position))
-
+        self.create_equations(impurity_text)
 
     def create_graph(self):
         plane = (
@@ -144,18 +106,25 @@ class CreateDecisionTree(Scene):
         )
         labels = VGroup(
             Text("Peso").scale(0.5).next_to(plane.x_axis.get_right(), UP),
-            Text("Altura").scale(0.5).next_to(plane.y_axis.get_top(), RIGHT).shift(0.2 * UP),
+            Text("Altura")
+            .scale(0.5)
+            .next_to(plane.y_axis.get_top(), RIGHT)
+            .shift(0.2 * UP),
         )
         dot1 = Dot(color=RED).scale(0.5)
-        text1 = Text("Basquete").scale(0.5).next_to(dot1, RIGHT)
-        
+        text1 = Text("Basquete", font="Roboto").scale(0.5).next_to(dot1, RIGHT)
+
         dot2 = Dot(color=BLUE).scale(0.5).next_to(dot1, DOWN)
-        text2 = Text("Sumô").scale(0.5).next_to(dot2, RIGHT)
-        
+        text2 = Text("Sumô", font="Roboto").scale(0.5).next_to(dot2, RIGHT)
+
         dot3 = Dot(color=GREEN).scale(0.5).next_to(dot2, DOWN)
-        text3 = Text("Jockey").scale(0.5).next_to(dot3, RIGHT)
-        
-        legend = VGroup(dot1, text1, dot2, text2, dot3, text3).next_to(plane, UP+RIGHT).shift(2 * DOWN)
+        text3 = Text("Jockey", font="Roboto").scale(0.5).next_to(dot3, RIGHT)
+
+        legend = (
+            VGroup(dot1, text1, dot2, text2, dot3, text3)
+            .next_to(plane, UP + RIGHT)
+            .shift(2 * DOWN)
+        )
 
         self.play(Create(plane), run_time=2)
         self.wait(0.5)
@@ -165,22 +134,26 @@ class CreateDecisionTree(Scene):
 
         return plane
 
-
     def add_points_to_graph(self, graph, points: list):
+        points = VGroup()
         for point in points:
             x, y, color = point
             dot = Dot(graph.coords_to_point(x, y), color=color)
             self.play(Create(dot), run_time=0.2)
-        
+            points.add(dot)
+
         self.wait()
 
+        return points
 
     def create_fisrt_tree(self, points: list):
+        tree = VGroup()
         # Root Node
         tree_root = RoundedRectangle(height=1, width=2.3, corner_radius=0.2)
         tree_root.set_fill(BLUE, opacity=0.5)
         tree_root.to_edge(UP, buff=1.5).to_edge(RIGHT, buff=3)
         self.play(Create(tree_root))
+        tree.add(tree_root)
 
         b, g, r = 0, 0, 0
         dots = []
@@ -188,45 +161,77 @@ class CreateDecisionTree(Scene):
             _, _, color = point
             if color == BLUE:
                 b += 1
-                dot = Dot(color=color).next_to(tree_root, RIGHT*b, buff=0.2).shift(UP*0.25)
+                dot = (
+                    Dot(color=color)
+                    .next_to(tree_root, RIGHT * b, buff=0.2)
+                    .shift(UP * 0.25)
+                )
             elif color == RED:
                 r += 1
-                dot = Dot(color=color).next_to(tree_root, RIGHT*r, buff=0.2)
+                dot = Dot(color=color).next_to(tree_root, RIGHT * r, buff=0.2)
             else:
                 g += 1
-                dot = Dot(color=color).next_to(tree_root, RIGHT*g, buff=0.2).shift(DOWN*0.25)
+                dot = (
+                    Dot(color=color)
+                    .next_to(tree_root, RIGHT * g, buff=0.2)
+                    .shift(DOWN * 0.25)
+                )
             dots.append(dot)
-        
+            tree.add(dot)
+
         self.play(AnimationGroup(*[Create(dot) for dot in dots]), run_time=0.5)
 
-        text = Text("Altura > 1,90", font="Roboto", font_size=20).move_to(tree_root)
-        self.play(Write(text))
+        text_height = Text("Altura > 1,90", font="Roboto", font_size=20).move_to(
+            tree_root
+        )
+        tree.add(text_height)
+        self.play(Write(text_height))
         self.wait()
 
-        # Node 1
+        # Leaf Node 1
         node1 = RoundedRectangle(height=1, width=2, corner_radius=0.2)
-        node1.set_fill(GREEN, opacity=0.5).next_to(tree_root, DOWN, buff=1.5).shift(LEFT*1.1)
-        arrow_r1 = Arrow(start=tree_root.get_bottom(), end=node1.get_top(), buff=0, max_tip_length_to_length_ratio=0.1, stroke_width=4)
-        arrow_text_r1 = Text("Sim", font="Roboto", font_size=20).next_to(arrow_r1, LEFT*0.3)
+        node1.set_fill(GREEN, opacity=0.5).next_to(tree_root, DOWN, buff=1.5).shift(
+            LEFT * 1.1
+        )
+        arrow_r1 = Arrow(
+            start=tree_root.get_bottom(),
+            end=node1.get_top(),
+            buff=0,
+            max_tip_length_to_length_ratio=0.1,
+            stroke_width=4,
+        )
+        arrow_text_r1 = Text("Sim", font="Roboto", font_size=20).next_to(
+            arrow_r1, LEFT * 0.3
+        )
         self.play(Create(node1), Create(arrow_r1), Write(arrow_text_r1))
-        
+
         r = 0
         dots = []
         for point in points:
             _, _, color = point
             if color == RED:
                 r += 1
-                dot = Dot(color=color).next_to(node1, LEFT*r, buff=0.2)
+                dot = Dot(color=color).next_to(node1, LEFT * r, buff=0.2)
                 dots.append(dot)
 
         self.play(AnimationGroup(*[Create(dot) for dot in dots]), run_time=0.5)
         self.wait()
 
-        # Node 2
+        # Leaf Node 2
         node2 = RoundedRectangle(height=1, width=2, corner_radius=0.2)
-        node2.set_fill(BLUE, opacity=0.5).next_to(tree_root, DOWN, buff=1.5).shift(RIGHT*1.1)
-        arrow_r2 = Arrow(start=tree_root.get_bottom(), end=node2.get_top(), buff=0, max_tip_length_to_length_ratio=0.1, stroke_width=4)
-        arrow_text_r2 = Text("Não", font="Roboto", font_size=20).next_to(arrow_r2, RIGHT*0.4)
+        node2.set_fill(BLUE, opacity=0.5).next_to(tree_root, DOWN, buff=1.5).shift(
+            RIGHT * 1.1
+        )
+        arrow_r2 = Arrow(
+            start=tree_root.get_bottom(),
+            end=node2.get_top(),
+            buff=0,
+            max_tip_length_to_length_ratio=0.1,
+            stroke_width=4,
+        )
+        arrow_text_r2 = Text("Não", font="Roboto", font_size=20).next_to(
+            arrow_r2, RIGHT * 0.4
+        )
         self.play(Create(node2), Create(arrow_r2), Write(arrow_text_r2))
 
         b, g = 0, 0
@@ -235,11 +240,19 @@ class CreateDecisionTree(Scene):
             _, _, color = point
             if color == BLUE:
                 b += 1
-                dot = Dot(color=color).next_to(node2, RIGHT*b, buff=0.2).shift(UP*0.125)
+                dot = (
+                    Dot(color=color)
+                    .next_to(node2, RIGHT * b, buff=0.2)
+                    .shift(UP * 0.125)
+                )
                 dots.append(dot)
             elif color == GREEN:
                 g += 1
-                dot = Dot(color=color).next_to(node2, RIGHT*g, buff=0.2).shift(DOWN*0.125)
+                dot = (
+                    Dot(color=color)
+                    .next_to(node2, RIGHT * g, buff=0.2)
+                    .shift(DOWN * 0.125)
+                )
                 dots.append(dot)
 
         self.play(AnimationGroup(*[Create(dot) for dot in dots]), run_time=0.5)
@@ -247,12 +260,22 @@ class CreateDecisionTree(Scene):
         text = Text("Peso > 100", font="Roboto", font_size=20).move_to(node2)
         self.play(Write(text))
         self.wait()
-        
-        # Node 3
+
+        # Leaf Node 3
         node3 = RoundedRectangle(height=1, width=2, corner_radius=0.2)
-        node3.set_fill(GREEN, opacity=0.5).next_to(node2, DOWN, buff=1.5).shift(LEFT*1.1)
-        arrow_r3 = Arrow(start=node2.get_bottom(), end=node3.get_top(), buff=0, max_tip_length_to_length_ratio=0.1, stroke_width=4)
-        arrow_text_r3 = Text("Sim", font="Roboto", font_size=20).next_to(arrow_r3, LEFT*0.3)
+        node3.set_fill(GREEN, opacity=0.5).next_to(node2, DOWN, buff=1.5).shift(
+            LEFT * 1.1
+        )
+        arrow_r3 = Arrow(
+            start=node2.get_bottom(),
+            end=node3.get_top(),
+            buff=0,
+            max_tip_length_to_length_ratio=0.1,
+            stroke_width=4,
+        )
+        arrow_text_r3 = Text("Sim", font="Roboto", font_size=20).next_to(
+            arrow_r3, LEFT * 0.3
+        )
         self.play(Create(node3), Create(arrow_r3), Write(arrow_text_r3))
 
         b = 0
@@ -261,17 +284,27 @@ class CreateDecisionTree(Scene):
             _, _, color = point
             if color == BLUE:
                 b += 1
-                dot = Dot(color=color).next_to(node3, LEFT*b, buff=0.2)
+                dot = Dot(color=color).next_to(node3, LEFT * b, buff=0.2)
                 dots.append(dot)
 
         self.play(AnimationGroup(*[Create(dot) for dot in dots]), run_time=0.5)
         self.wait()
 
-        # Node 4
+        # Leaf Node 4
         node4 = RoundedRectangle(height=1, width=2, corner_radius=0.2)
-        node4.set_fill(GREEN, opacity=0.5).next_to(node2, DOWN, buff=1.5).shift(RIGHT*1.1)
-        arrow_r4 = Arrow(start=node2.get_bottom(), end=node4.get_top(), buff=0, max_tip_length_to_length_ratio=0.1, stroke_width=4)
-        arrow_text_r4 = Text("Não", font="Roboto", font_size=20).next_to(arrow_r4, RIGHT*0.4)
+        node4.set_fill(GREEN, opacity=0.5).next_to(node2, DOWN, buff=1.5).shift(
+            RIGHT * 1.1
+        )
+        arrow_r4 = Arrow(
+            start=node2.get_bottom(),
+            end=node4.get_top(),
+            buff=0,
+            max_tip_length_to_length_ratio=0.1,
+            stroke_width=4,
+        )
+        arrow_text_r4 = Text("Não", font="Roboto", font_size=20).next_to(
+            arrow_r4, RIGHT * 0.4
+        )
         self.play(Create(node4), Create(arrow_r4), Write(arrow_text_r4))
 
         g = 0
@@ -280,9 +313,76 @@ class CreateDecisionTree(Scene):
             _, _, color = point
             if color == GREEN:
                 g += 1
-                dot = Dot(color=color).next_to(node4, RIGHT*g, buff=0.2)
+                dot = Dot(color=color).next_to(node4, RIGHT * g, buff=0.2)
                 dots.append(dot)
 
         self.play(AnimationGroup(*[Create(dot) for dot in dots]), run_time=0.5)
+
+        # Classification
+        text = Text("Basquete", font="Roboto", font_size=20).move_to(node1)
+        self.play(Write(text))
+        self.wait()
+        text = Text("Sumô", font="Roboto", font_size=20).move_to(node3)
+        self.play(Write(text))
+        self.wait()
+        text = Text("Jockey", font="Roboto", font_size=20).move_to(node4)
+        self.play(Write(text))
         self.wait(3)
 
+    def create_equations(self, impurity_text):
+        entropy_text = (
+            Text("Entropia", font_size=35)
+            .next_to(impurity_text, DOWN * 2.5, buff=0.5)
+            .shift(LEFT * 2)
+        )
+
+        gini_text = (
+            Text("Gini", font_size=35)
+            .next_to(impurity_text, DOWN * 2.5, buff=0.5)
+            .shift(RIGHT * 2)
+        )
+
+        arrow_entropy = Arrow(
+            start=impurity_text.get_bottom(),
+            end=entropy_text.get_top(),
+            buff=0.2,
+            max_tip_length_to_length_ratio=0.1,
+            stroke_width=4,
+        )
+
+        arrow_gini = Arrow(
+            start=impurity_text.get_bottom(),
+            end=gini_text.get_top(),
+            buff=0.2,
+            max_tip_length_to_length_ratio=0.1,
+            stroke_width=4,
+        )
+        self.play(
+            Create(arrow_entropy),
+            Create(arrow_gini),
+            Write(entropy_text),
+            Write(gini_text),
+        )
+        self.wait()
+
+        entropy_equation = (
+            MathTex("E(S) = -\\sum_{i=1}^{c} p_i \\log_2(p_i)")
+            .scale(0.7)
+            .next_to(entropy_text, DOWN * 1.5)
+        )
+
+        gini_equation = (
+            MathTex("G(S) = 1 - \\sum_{i=1}^{c} p_i^2")
+            .scale(0.7)
+            .next_to(gini_text, DOWN * 1.5)
+        )
+
+        self.play(Write(entropy_equation), Write(gini_equation))
+        self.wait(3)
+
+    # Create a function that clears the screen
+    def clear_screen(self, obj=None):
+        if not obj:
+            self.play(*[FadeOut(mob) for mob in self.mobjects])
+        else:
+            self.play(*[FadeOut(mob) for mob in obj])
